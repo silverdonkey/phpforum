ae4642ad40b21e431f25ec7863ce9f61ab15bda0
Nikolay Orozov <dev@niko-consulting.de>
2019-10-28 16:37:48 +0100

    refactored

--- README.md
+++ README.md
@@ -3,9 +3,13 @@
 php based forum
 
 # Dev Environment
+Start Docker Container (to rebuild use '--build' option):
+- docker-compose up -d
+- docker-compose up --build -d
 
-- Start Docker Container (to rebuild use '--build' option):
-  docker-compose up -d
+Stop Docker Containter 
+- docker-compose down
 
-- Stop Docker Containter:
-  docker-compose down
+# Init Databse 
+Use init script
+- Run database-init.sql 
--- src/index.php
+++ src/index.php
@@ -1,11 +1,15 @@
 <?php
+
 spl_autoload_register(function ($name) {
   $parts = explode('\\', $name);
   $parts[0] = $parts[0] == 'JokeSite' ? 'Model' : $parts[0];
   require './' . implode(DIRECTORY_SEPARATOR, $parts) . '.php';
 });
+
 require 'db/config.php';
+
 $pdo = new \Pdo('mysql:host=' . DB_HOST . ';dbname=' . DB_SCHEMA, DB_USER, DB_PASS, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
 $route = $_GET['route'] ?? '';
 if ($route == '') {
   $model = new \Model\Posts($pdo);
@@ -14,4 +18,5 @@ if ($route == '') {
   http_response_code(404);
   echo 'Page not found (Invalid route)';
 }
+
 echo $view->output($model);
